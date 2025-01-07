77c4e74a5c09ccb3d3a3fad9fe409742bb52c95f
Gugulothu Narahari <NarahariG@pyramidconsultinginc.com>
2025-01-07 20:06:39 +0530

    dsgds

--- src/main/java/com/test/boot/config/SingletonAndPrototypeTest.java
+++ src/main/java/com/test/boot/config/SingletonAndPrototypeTest.java
@@ -4,18 +4,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class SingletonAndPrototypeTest {
 
-//    public static void main(String[] args) throws InterruptedException {
-//        AnnotationConfigApplicationContext context
-//                = new AnnotationConfigApplicationContext(AppConfig.class);
-//
-//        SingletonBean firstSingleton = context.getBean(SingletonBean.class);
-//        PrototypeBean firstPrototype = firstSingleton.getPrototypeBean();
-//
-//        // get singleton bean instance one more time
-//        SingletonBean secondSingleton = context.getBean(SingletonBean.class);
-//        PrototypeBean secondPrototype = secondSingleton.getPrototypeBean();
-//
-//        System.out.println(firstPrototype==secondPrototype);
-//
-//    }
+    public static void main(String[] args) throws InterruptedException {
+        AnnotationConfigApplicationContext context
+                = new AnnotationConfigApplicationContext();
+
+        SingletonBean firstSingleton = context.getBean(SingletonBean.class);
+        PrototypeBean firstPrototype = firstSingleton.getPrototypeBean();
+
+        // get singleton bean instance one more time
+        SingletonBean secondSingleton = context.getBean(SingletonBean.class);
+        PrototypeBean secondPrototype = secondSingleton.getPrototypeBean();
+
+        System.out.println(firstPrototype==secondPrototype);
+
+    }
 }
